aba31b3cdf13c16cae9c2fb51209921dfce44454
CruddyCorder <sasha_sidorov_2004@bk.ru>
2024-10-23 17:21:32 +0300

    Add product_of_odd_numerals

---
+++ product_of_odd_numerals.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int number, flag = 1;
+    int count = 0;
+    int product = 1;
+    int has_odd = 0;
+
+    while (flag) {
+        if (scanf("%d", &number) != 1) {
+            flag = 0;
+        } else if (number == -1) {
+            flag = 0;
+        } else {
+            count++;
+            if (number % 2 != 0) {
+                product *= number;
+                has_odd = 1;
+            }
+        }
+    }
+
+    if (count == 0) {
+        printf("0");
+    } else if (count == 1) {
+        printf("%d", number < 0 ? -number : number);
+    } else if (count == 2) {
+        printf("0");
+    } else {
+        printf("%d", has_odd ? product : 0);
+    }
+
+    return 0;
+}
